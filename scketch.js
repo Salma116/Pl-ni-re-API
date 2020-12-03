b416460577e253bc656b0b1f9d6365fc20c7a354
manon-coulombe <manon.coulombe@student.adatechschool.fr>
2020-12-03 12:14:53 +0100

    commit

--- scketch.js
+++ scketch.js
@@ -5,5 +5,19 @@ request.responseType = 'json';
 request.send();
 request.onload = function() {
     var recette = request.response;
-    console.log(recette[0].strMeal)
+    console.log(recette)
+    console.log(recette.meals[0].strMeal)
+  }
+
+var header = document.querySelector('header');
+console.log(header)
+
+  function populateHeader(recette) {
+    var myH1 = document.createElement('h1');
+    myH1.textContent = recette.meals[0].strMeal;
+    document.header.appendChild(myH1);
+
+  //  var myPara = document.createElement('p');
+  //  myPara.textContent = 'Hometown: ' + jsonObj['homeTown'] + jsonObj['formed'];
+  //  header.appendChild(myPara);
   }
