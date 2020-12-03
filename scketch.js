d6f5896beb3a104c2438a521a1b457b0afe2eb52
josiane Béké <josybekette@gmail.com>
2020-12-03 12:33:48 +0100

    recuperer element

--- scketch.js
+++ scketch.js
@@ -7,6 +7,7 @@ request.onload = function() {
     var recette = request.response;
     console.log(recette)
     console.log(recette.meals[0].strMeal)
+     populateHeader(recette);
   }
 
 var header = document.querySelector('header');
@@ -15,9 +16,7 @@ console.log(header)
   function populateHeader(recette) {
     var myH1 = document.createElement('h1');
     myH1.textContent = recette.meals[0].strMeal;
-    document.header.appendChild(myH1);
+    header.appendChild(myH1);
 
-  //  var myPara = document.createElement('p');
-  //  myPara.textContent = 'Hometown: ' + jsonObj['homeTown'] + jsonObj['formed'];
-  //  header.appendChild(myPara);
+  
   }
